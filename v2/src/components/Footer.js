b1fd1113dd744dc813f9ad7616fbdd859279332d
Shane Crosby <iam@shanecrosby.com>
2023-11-05 20:16:30 +0800

    Moved address block to about me section

--- v2/src/components/Footer.js
+++ v2/src/components/Footer.js
@@ -2,34 +2,12 @@
 
 import React from 'react';
 import { Link } from 'gatsby';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faXTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
     return (
         <footer>
             {/* Section 1 - Left: */}
             <div className="footer-container">
-                <div className='footer-bg'>
-                    <div className="footer-left">
-                        <p className='address-block'>
-                            2 Cortona Grove<br />
-                            Sinagra, Western Australia, 6065<br />
-                            <b>Phone:</b> +61 407 472 944<br />
-                            <b>ABN:</b> 41781010072<br />
-                        </p>
-                    
-                        {/* Social Buttons: */}
-                            <ul className='social-icons'>
-                                <a href="https://facebook.com/crosbyshane" aria-label="Facebook" target="_blank" rel="noreferrer"><li><FontAwesomeIcon icon={faFacebook} /></li></a>
-                                <a href="https://twitter.com/crosbyshane" aria-label="Twitter" target="_blank" rel="noreferrer"><li><FontAwesomeIcon icon={faXTwitter} /></li></a>
-                                <a href="https://instagram.com/crosbyshane" aria-label="Instagram" target="_blank" rel="noreferrer"><li><FontAwesomeIcon icon={faInstagram} /></li></a>
-                                <a href="https://www.youtube.com/@ShaneCrosby" aria-label="Youtube" target="_blank" rel="noreferrer"><li><FontAwesomeIcon icon={faYoutube} /></li></a>
-                                <a href="tel:+61407472944" title="+61407472944" aria-label="Telephone" target="_blank" rel="noreferrer"><li><FontAwesomeIcon icon={faPhone} /></li></a>
-                            </ul>
-                    </div>
-                </div>
                 <div className="footer-bar">
                         <p>&copy; 2023 Shane Crosby. All Rights Reserved. | <Link to="/">Privacy Policy</Link> | <Link to="/">Terms of Service</Link></p>
                 </div>
