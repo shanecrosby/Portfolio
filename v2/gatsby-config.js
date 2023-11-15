eca7d06a561f823df3efbb9224c8865159f3311d
Shane Crosby <iam@shanecrosby.com>
2023-11-15 14:59:51 +0800

    Switched font for readability

--- v2/gatsby-config.js
+++ v2/gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
-        "fonts": [`Montserrat`,`Kanit`,`Shadows Into Light`,`Architects Daughter`,`Square Peg`],
+        "fonts": [`Montserrat`,`Kanit`,`Shadows Into Light`,`Architects Daughter`,`Handlee`],
         "display": 'swap'
       }
     },
