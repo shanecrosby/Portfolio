4df0837cf414e8942a8fa330eec22147221856df
Shane Crosby <iam@shanecrosby.com>
2023-11-15 15:46:18 +0800

    Added button to site card

--- v2/src/components/sitecard.js
+++ v2/src/components/sitecard.js
@@ -9,12 +9,17 @@ import BlockContent from '@sanity/block-content-to-react'
 const SiteCardComponent = ({ examplesite, transitioning }) => {
     const testimonialClass = examplesite._rawTestimonial ? 'testimonial-card' : 'testimonial-card hidden';
     const cardClass = transitioning ? `${testimonialClass} transitioning` : testimonialClass;
+    let siteButtonVisible = ''
+    if (examplesite.url === '#') {
+        siteButtonVisible = 'hidden'
+    } else { siteButtonVisible = '' }
 
     return (
         <div className={`card-container ${transitioning ? 'fade-out' : 'fade-in'}`}>
             <div className="site-card">
                 <div className="sitecard-header"><h1><a href={examplesite.url} target='_blank' rel='noreferrer'>{examplesite.name}</a></h1></div>
                 <div className="sitecard-descriptor"><BlockContent blocks={examplesite._rawDescription} /></div>
+                <a href={examplesite.url} target='_blank' rel='noreferrer' ><div className={`link-button ${siteButtonVisible}`}>Visit Site</div></a>
             </div>
             <div className={cardClass}> 
                 <div className="testimonial-header"><h1>{examplesite.clientname}</h1></div>
